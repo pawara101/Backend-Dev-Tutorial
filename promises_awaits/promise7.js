cf4cc20a150a092d86ba6ba3bb3f68b5c712c493
pawara101 <pawara101dassanayake@gmail.com>
2022-12-11 01:27:41 +0530

    version 2 - promise7

--- promises_awaits/promise7.js
+++ promises_awaits/promise7.js
@@ -1,2 +1,22 @@
 const fs = require('fs');
 const util = require('util');
+
+// with promise
+var read = util.promisify(fs.readFile);
+Promise.all([
+    read('customer1.txt'),
+    read('customer2.txt'),
+    read('customer3.txt')
+])
+
+    .then(data =>{
+        const [data1,data2,data3] = data
+
+        console.log(data1.toString());
+        console.log(data2.toString());
+        console.log(data3.toString());
+    })
+
+    .catch(err => {
+        console.log(err);
+    });
