684f4d1008868dc13300fe1e15e9e262269fde1f
pawara101 <pawara101dassanayake@gmail.com>
2022-12-11 01:22:33 +0530

    promise7

--- promises_awaits/promise6.js
+++ promises_awaits/promise6.js
@@ -1,1 +1,15 @@
 const fs = require('fs');
+const util = require('util');//support internal APIs
+
+// Without promises
+fs.readFile('customer1.txt', (err, data) => {
+    console.log(data.toString());
+});
+
+// With promise -- using util
+var read = util.promisify(fs.readFile);
+read('customer1.txt')
+    .then(data => {
+        console.log(data.toString());
+    })
+    .catch(err => alert(err));
